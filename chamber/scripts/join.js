28f1b526f13347f5131f872b31d5990122c8397d
Adriano-Mori <amorihuamani@byupathway.edu>
2026-01-31 14:26:16 -0500

    polished version of join.html contains card animations and dialog info, styles are pending

---
+++ chamber/scripts/join.js
@@ -0,0 +1,26 @@
+const modal = document.querySelectorAll('.open-modal')
+const closeModal = document.querySelectorAll('.close-modal')
+modal.forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        const dialog = document.querySelector(link.getAttribute("href"));
+        dialog.showModal();
+    });
+});
+
+closeModal.forEach(button => {
+    button.addEventListener("click", () => {
+        button.closest("dialog").close();
+    });
+});
+const dialogs = document.querySelectorAll("dialog");
+
+dialogs.forEach(dialog => {
+    dialog.addEventListener("click", (e) => {
+        if (e.target === dialog) {
+            dialog.close();
+        }
+    });
+});
+
+document.querySelector("#timestamp").value = new Date().toISOString();
