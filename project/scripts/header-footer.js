ae9078e68593202afac0be42efb70b3f3415057c
Adriano-Mori <amorihuamani@byupathway.edu>
2026-02-10 19:35:51 -0500

    homepage's structure was completed

--- project/scripts/header-footer.js
+++ project/scripts/header-footer.js
@@ -1,4 +1,3 @@
-
 const navButton = document.querySelector('#nav-btn');
 const navLinks = document.querySelector('#nav-bar');
 
@@ -6,3 +5,8 @@ navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navLinks.classList.toggle('show');
 });
+const yearSpan = document.getElementById("year");
+yearSpan.textContent = new Date().getFullYear();
+//last modification date
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = ` ${document.lastModified}`
