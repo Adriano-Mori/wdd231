30d9ef68db7d1a51f923ed4890a46173de50d20b
Adriano-Mori <amorihuamani@byupathway.edu>
2026-02-14 14:56:14 -0500

    CONTACT PAGE OFFICIALLY FINISHED ANIMATIONS ARE PENDING

--- project/scripts/toggle.js
+++ project/scripts/toggle.js
@@ -1,47 +1,37 @@
 const reservationFields = document.querySelector("#reservation-fields");
 const deliveryFields = document.querySelector("#delivery-fields");
-const extraInfo = document.getElementById("service-extra-info");
+
+const reservationInfo = document.getElementById("reservation-info");
+const deliveryInfo = document.getElementById("delivery-info");
+
 document.querySelectorAll('input[name="serviceType"]').forEach(radio => {
     radio.addEventListener("change", (e) => {
-
         if (e.target.value === "reservation") {
+
             reservationFields.style.display = "block";
             deliveryFields.style.display = "none";
-            extraInfo.innerHTML = `
-                <section class="locations-info">
-                    <h3>Available Locations</h3>
-                    <ul>
-                        <li>Lima</li>
-                        <li>Trujillo</li>
-                        <li>Arequipa</li>
-                    </ul>
-                </section>
-            `;
 
-            // Required control
+
+            reservationInfo.style.display = "flex";
+            deliveryInfo.style.display = "none";
+
+
             document.getElementById("date").required = true;
             document.getElementById("numberPeople").required = true;
             document.getElementById("address").required = false;
-
         } else {
+
             reservationFields.style.display = "none";
             deliveryFields.style.display = "block";
-            extraInfo.innerHTML = `
-                <section class="delivery-options">
-                <h2>Order through our delivery partners</h2>
-                <img src="images/glovo.png" >
-                <img src="images/pedidosya.png" >
-                <img src="images/ubereats.png" >
-                <img src="images/llamafood.png" >
-                <img src="images/rappi.png" >
-                </section>`;
+
+
+            reservationInfo.style.display = "none";
+            deliveryInfo.style.display = "block";
+
 
             document.getElementById("date").required = false;
             document.getElementById("numberPeople").required = false;
             document.getElementById("address").required = true;
         }
-
     });
 });
-
-
